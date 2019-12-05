3368976c7f4588b27edeae0d46304c474cb2c1d3
Halilcan <halilc.gunduz@gmail.com>
2019-12-05 16:16:57 +0300

    t

--- Flask/app.py
+++ Flask/app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import json
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/sign', methods=['POST'])
+
+def sign():
+    # read the posted values from the UI
+    # req_data = request.get_json()
+    # name = req_data['name']
+    # return name
+    #if request.method == 'POST':
+        #print(request.form.get('name'))
+        #return jsonify(request.form.get('name'))
+    # data = jsonify(request.data)
+    # print(data)
+    # data = json.loads(str(request.data))
+
+    data = request.data.decode('utf8')
+    print(data)
+    data = json.loads(data)
+    print(data['user_id'])
+
+    return request.data
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/login/', methods=["POST"])
+def login():
+    return request
